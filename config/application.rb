054103284d33e8b10e36f776963e50476502b68a
Ali Shanaakh <ashanaakh@gmail.com>
2019-02-25 17:22:22 +0200

    Fix: Add missing action mailer

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require_relative 'boot'
 
 require 'rails'
 
-%w( active_record action_controller action_view sprockets ).each { |framework| require "#{framework}/railtie" }
+%w( active_record action_controller action_mailer action_view sprockets ).each { |framework| require "#{framework}/railtie" }
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
